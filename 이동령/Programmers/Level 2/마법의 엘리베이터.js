504eb8c860c1eccc797b25a844593603a0d846b4
DongRyeong Lee <79556112+LellowMellow@users.noreply.github.com>
2024-01-07 08:33:44 +0900

    Create 마법의 엘리베이터.js

---
+++ 이동령/Programmers/Level 2/마법의 엘리베이터.js
@@ -0,0 +1,14 @@
+function solution(storey) {
+    let result = 0;
+    while(storey) {
+        let current = storey % 10;
+        storey = (storey - current) / 10;
+        let next = storey % 10;
+        if(current < 5 || (current === 5 && next < 5)) result += current;
+        else {
+            result += (10 - current);
+            storey++;
+        }
+    }
+    return result;
+}
