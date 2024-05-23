ecbcbb174b1f6710124f967004bd2e8b219879b2
Byeonjin <quswlstkd88@naver.com>
2024-05-24 01:54:52 +0900

    24-05-24 컬러볼 풀이 완료
    - 정렬 및 누적합 저장을 통한 풀이

--- 변진상/Baekjoon/10800_컬러볼.js
+++ 변진상/Baekjoon/10800_컬러볼.js
@@ -1,5 +1,4 @@
 const fs = require("fs");
-let maxV = 0;
 const stdin = fs
   .readFileSync(
     "/Users/byeonjinsang/Desktop/team528_PS/변진상/Baekjoon/case.txt"
@@ -9,7 +8,6 @@ const stdin = fs
   .split("\n")
   .map((ele) =>
     ele.split(" ").map((ele, idx, arr) => {
-      if (idx === 1) maxV = Math.max(maxV, +ele);
       return +ele;
     })
   );
@@ -17,29 +15,21 @@ const stdin = fs
 const N = stdin[0][0];
 const ballsArr = stdin.slice(1);
 
-ballsArr;
+ballsArr.forEach((ele, idx) => ele.push(idx));
+ballsArr.sort((a, b) => a[1] - b[1]);
 
-const sumArr = Array.from({ length: N + 1 }, () =>
-  Array.from({ length: maxV + 1 }, () => 0)
-);
+const colorSum = new Array(N + 1).fill(0);
+const answer = new Array(N + 1).fill(0);
+let sum = 0;
+let lastBallIdx = 0;
 
-const sortedBallsArr = [...ballsArr].sort((a, b) => a[1] - b[1]);
-
-sortedBallsArr.forEach(([color, value]) => {
-  for (let i = value + 1; i <= maxV; i++) {
-    sumArr[color][i] += value;
+ballsArr.forEach(([color, value, originalIdx], idx) => {
+  while (value > ballsArr[lastBallIdx][1]) {
+    sum += ballsArr[lastBallIdx][1];
+    colorSum[ballsArr[lastBallIdx][0]] += ballsArr[lastBallIdx][1];
+    lastBallIdx += 1;
   }
-});
-
-const answer = [];
-
-ballsArr.forEach(([color, value]) => {
-  let sum = 0;
-  for (let i = 1; i <= N; i++) {
-    if (color === i) continue;
-    sum += sumArr[i][value];
-  }
-  answer.push(sum);
+  answer[originalIdx] = sum - colorSum[color];
 });
 
 console.log(answer.join("\n"));
